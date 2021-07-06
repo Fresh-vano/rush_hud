deea3762d8c48f9e8d75b97192d821a031e85094
Fresh-vano <vano94600@gmail.com>
2021-07-06 11:53:36 +0300

    Delete team name

--- src/HUD/MatchBar/MatchBar.tsx
+++ src/HUD/MatchBar/MatchBar.tsx
@@ -200,7 +200,6 @@ export default class TeamBox extends React.Component<IProps, IState> {
           </div>
           <div className={`timer ${winState.show ? "win" : ""}`}>
             <div className={`WinIndicator ${winState.show ? "show" : ""} ${winState.side === "right" ? `${right.side}` : `${left.side}`}`}>
-              <div id="win_team">{winState.side === "right" ? right.name : left.name}</div>
               <div id="Win">WINS</div>
               <div id="under_line">the round</div>
             </div>
