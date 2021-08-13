e5a109b5955d7d2ab9a7308a1ba0a308c47d2365
Fresh-vano <vano94600@gmail.com>
2021-08-13 13:54:48 +0300

    start work

--- src/HUD/AdsBox/AdsBox.tsx
+++ src/HUD/AdsBox/AdsBox.tsx
@@ -3,11 +3,12 @@ import './adsbox.scss'
 import {configs, actions} from './../../App';
 import isSvg from '../isSvg';
 
-export default class AdsBox extends React.Component<any, {image?: string, show: boolean}>{
+export default class AdsBox extends React.Component<any, {image: Array<string>, show: boolean, quantity?:number}>{
   constructor(props: any) {
 		super(props);
 		this.state = {
-      show: true
+      show: true,
+      image:["","","","","",""]
 		}
 	}
 
@@ -17,29 +18,115 @@ export default class AdsBox extends React.Component<any, {image?: string, show: 
     });
     configs.onChange((data:any) => {
       if(!data) return;
-      const adsbox = data.trivia;
+      const adsbox = data.ads_panel;
       if(!adsbox) return;
-      if(`button_image` in adsbox){
-        this.setState({image:adsbox[`button_image`]})
-    }
+      if(`quantity_ads` in adsbox){
+        this.setState({quantity:adsbox[`quantity_ads`]})
+      }
+      switch (this.state.quantity){
+        case 1:{
+          if(`ads_1` in adsbox){
+            this.setState({image:adsbox[`ads_1`][0]})
+          }
+          break;
+        }
+        case 2:{
+          if(`ads_1` in adsbox){
+          this.setState({image:adsbox[`ads_1`][0]})
+          }
+          if(`ads_2` in adsbox){
+            this.setState({image:adsbox[`ads_2`][1]})
+            }
+          break;
+        }
+        case 3:{
+          if(`ads_1` in adsbox){
+          this.setState({image:adsbox[`ads_1`]})
+          }
+          if(`ads_2` in adsbox){
+            this.setState({image:adsbox[`ads_2`]})
+          }
+          if(`ads_3` in adsbox){
+            this.setState({image:adsbox[`ads_3`]})
+          }
+          break;
+        }
+        case 4:{
+          if(`ads_1` in adsbox){
+          this.setState({image:adsbox[`ads_1`]})
+          }
+          if(`ads_2` in adsbox){
+            this.setState({image:adsbox[`ads_2`]})
+          }
+          if(`ads_3` in adsbox){
+            this.setState({image:adsbox[`ads_3`]})
+          }
+          if(`ads_4` in adsbox){
+            this.setState({image:adsbox[`ads_4`]})
+          }
+          break;
+        }
+        case 5:{
+          if(`ads_1` in adsbox){
+          this.setState({image:adsbox[`ads_1`]})
+          }
+          if(`ads_2` in adsbox){
+            this.setState({image:adsbox[`ads_2`]})
+          }
+          if(`ads_3` in adsbox){
+            this.setState({image:adsbox[`ads_3`]})
+          }
+          if(`ads_4` in adsbox){
+            this.setState({image:adsbox[`ads_4`]})
+          }
+          if(`ads_5` in adsbox){
+            this.setState({image:adsbox[`ads_5`]})
+          }
+          break;
+        }
+        case 6:{
+          if(`ads_1` in adsbox){
+          this.setState({image:adsbox[`ads_1`]})
+          }
+          if(`ads_2` in adsbox){
+            this.setState({image:adsbox[`ads_2`]})
+          }
+          if(`ads_3` in adsbox){
+            this.setState({image:adsbox[`ads_3`]})
+          }
+          if(`ads_4` in adsbox){
+            this.setState({image:adsbox[`ads_4`]})
+          }
+          if(`ads_5` in adsbox){
+            this.setState({image:adsbox[`ads_5`]})
+          }
+          if(`ads_6` in adsbox){
+            this.setState({image:adsbox[`ads_6`]})
+          }
+          break;
+        }
+      }
+      console.log(this.state.image[adsbox[`ads_1`]])
     });
   }
 
   render(){
-    const { image, show} = this.state;
-    const encoding = image && isSvg(Buffer.from(image, 'base64')) ? 'svg+xml':'png';
+    const { image, show, quantity} = this.state;
+    const encoding = image && isSvg(Buffer.from(image[0], 'base64')) ? 'svg+xml':'png';
     return(
-      <div className="league_box">
+      <div className={`league_box ${quantity}`}>
         <div className="image_container">
-          {this.state.image ? <img src={`data:image/${encoding};base64,${image}`}/>:''}
+          {image ? <img src={`data:image/${encoding};base64,${image}`}/>:''}
         </div>
         <div className={`image ${show ? "show" : "hide"}`}>
-          <div className='two'></div>
-          <div className='three'></div>
-          <div className='four'></div>
-          <div className='five'></div>
-          <div className='six'></div>
-          <div className='seven'></div>
+          <div className='ads_one'>
+            {image ? <img src={`data:image/${encoding};base64,${image}`}/>:''}
+          </div>
+          <div className='ads_two'></div>
+          <div className='ads_three'></div>
+          <div className='ads_four'></div>
+          <div className='ads_five'></div>
+          <div className='ads_six'></div>
         </div>
       </div>
     );
